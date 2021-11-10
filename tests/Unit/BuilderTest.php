f9d369c2af86807fbc62afd02d04e6b6d7e5222e
Matteo Meloni <matteomelonig@gmail.com>
2021-11-10 14:50:21 +0100

    added tests for query builder code refactoring

--- tests/Unit/BuilderTest.php
+++ tests/Unit/BuilderTest.php
@@ -3,7 +3,6 @@
 namespace Matteomeloni\CloudwatchLogs\Tests\Unit;
 
 use Matteomeloni\CloudwatchLogs\Builder;
-use Matteomeloni\CloudwatchLogs\Client\Mock\Client;
 use Matteomeloni\CloudwatchLogs\CloudWatchLogs;
 use Matteomeloni\CloudwatchLogs\Tests\TestCase;
 
@@ -340,31 +339,5 @@ class BuilderTest extends TestCase
             'function' => 'avg',
             'column' => 'foo'
         ], $this->builder->aggregates);
-    }
-
-    /**
-     * @return CloudWatchLogs|__anonymous@9509
-     */
-    protected function getFakeModel()
-    {
-        return new class extends CloudWatchLogs {
-            protected string $logGroupName = 'Test';
-            protected string $logStreamName = 'test';
-        };
-    }
-
-    /**
-     * @param $fakeModel
-     * @return Builder
-     */
-    protected function getBuilder($fakeModel): Builder
-    {
-        return new class($fakeModel) extends Builder {
-            public function __construct(CloudWatchLogs $fakeModel)
-            {
-                $this->model = $fakeModel;
-                $this->client = new Client($this->model->getLogGroupName(), $this->model->getLogStreamName());
-            }
-        };
     }
 }
